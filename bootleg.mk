1df1336c3cbb8e78a92c2edda09be779aee421e4
Jens Lody <jens@jenslody.de>
2018-09-02 23:04:11 +0200

    Bootleggers build fix

--- bootleg.mk
+++ bootleg.mk
@@ -7,7 +7,6 @@ TARGET_SCREEN_HEIGHT := 1280
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
-$(call inherit-product, vendor/bootleggers/config/caf_fw.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
